e1118a48c784a2da02bc5629107f59b51a99f6d8
Lesley Liang <lesley.liang@jarvish.com>
2017-03-13 14:36:36 +0800

    Initial Commit

---
+++ ScanBarcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScanBarcode
+//
+//  Created by Lesley Liang on 2017/3/13.
+//  Copyright © 2017年 Lesley Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScanBarcode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScanBarcode
+//
+//  Created by Lesley Liang on 2017/3/13.
+//  Copyright © 2017年 Lesley Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
